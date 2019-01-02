82015a2c9b20b72ff1e95ffe900adab89d588581
sidikriders <sidik@xwork.id>
2019-01-02 19:43:47 +0700

    create header and search functionality from header

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,15 +1,15 @@
-
-// intiate header select
-$('.h-select .btn').click(function(e) {
-  var options = $(e.currentTarget).parent().find('.options')
-  options.toggle()
-  options.find('p').off('click').click(function(e2) {
-    var opt = $(e2.currentTarget)
-    var _text = opt.text()
-    var val = opt.attr('val')
-    var placeholder = $(e.currentTarget).find('span')
-    placeholder.text(_text)
-    $(e.currentTarget).parent().attr('val', val)
-    options.hide()
-  })
+// initiate search header
+$('#header-search').on('keyup', function (e) {
+  if (e.keyCode === 13) {
+    return searchFromHeader()
+  }
 })
+
+$('#header .header-input .btn.info-btn').click(function (e) {
+  return searchFromHeader()
+})
+
+function searchFromHeader () {
+  var keyword = $('#header-search').val()
+  window.location.href = '/cari-mobil?q=' + keyword
+}
