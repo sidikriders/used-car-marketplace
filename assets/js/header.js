0829b640dc9d2603ccb7e1c23c2369fb4c1cb9e6
sidikriders <sidik@xwork.id>
2019-01-02 20:24:39 +0700

    add side menu

--- assets/js/header.js
+++ assets/js/header.js
@@ -13,3 +13,16 @@ function searchFromHeader () {
   var keyword = $('#header-search').val()
   window.location.href = '/cari-mobil?q=' + keyword
 }
+
+// initiate side menu
+$('#header .header-side-icon').click(function (e) {
+  var headerInput = $('#header .header-input')
+  var sideMenu = $('#side-menu')
+  if (sideMenu.hasClass('open')) {
+    sideMenu.removeClass('open')
+    headerInput.show()
+  } else {
+    sideMenu.addClass('open')
+    headerInput.hide()
+  }
+})
