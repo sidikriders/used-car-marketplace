ffcb20258b706e1b89eac0f0d1d4826ba035cd78
sidikriders <sidik@xwork.id>
2019-01-06 21:00:13 +0700

    add other filter in home search engine

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,13 @@
+// initiate input search engine
+$('form#keyword').submit(function (e) {
+  e.preventDefault()
+  var _form = $(e.currentTarget)
+  var input = _form.find('input')
+  var keyword = input.val()
+  var _url = '/cari-mobil'
+  window.location.href = '/cari-mobil' + keyword
+})
+
 // initiate select search engine
 $('#search-engine .input.select .btn').click(function (e) {
   var btn = $(e.currentTarget)
@@ -28,5 +38,47 @@ $('#search-engine .input.select .btn').click(function (e) {
     body.removeClass('side-menu-open')
     options.removeClass('show')
   })
+})
 
+// initiate tombol cari mobil
+$('#do-search').click(function (e) {
+  var keyword = $('#keyword input').val()
+  var locationId = Number($('#location').attr('val') || '0')
+  var locationName = $('#location .btn span').text()
+
+  var params = {
+    keyword: keyword ? 'q=' + keyword : false,
+    location: locationId > 0 ? ( 'location=' + locationName + '&locId=' + locationId) : false
+  }
+
+  window.location.href = '/cari-mobil?' + Object.keys(params).map(function (key) {
+    return params[key]
+  }).filter(function(key) {
+    return key
+  }).join('&')
+})
+
+// initiate autocomplete
+$('#search-engine .input.autocomplete input').on('focus', function(e) {
+  var _input = $(e.currentTarget)
+  var inputAutoComplete = _input.parent()
+  var options = inputAutoComplete.find('.options')
+  options.addClass('show')
+
+  _input.off('focusout').on('focusout', function (e) {
+    options.removeClass('show')
+  })
+
+  _input.off('keyup').on('keyup', function (e2) {
+    var isEnter = e2.keyCode === 13
+    if (isEnter) {
+      options.removeClass('show')
+      var selectedOpt = options.find('.opt:first')
+      inputAutoComplete.attr('val', selectedOpt.attr('val'))
+      _input.val(selectedOpt.text())
+      $('#year input').focus()
+    } else {
+      console.log('requqest autocomplete untuk ' + _input.val())
+    }
+  })
 })
