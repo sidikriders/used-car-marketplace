cc20b92b6a2d7e899f1821375906c9d37fadfe38
sidikriders <sidik@xwork.id>
2019-01-08 15:53:57 +0700

    add session in express

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+
+const appConfig = require('./app-config.js')
+const adminController = require('./controllers/admin-controller')
+const apiController = require('./controllers/api-controller')
 
 const app = express()
 
 app.use(express.static('./assets'))
 app.set('view engine', 'pug')
 app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.enable('trust-proxy')
+app.use(session({
+  secret: appConfig.session.secret,
+  resave: false,
+  saveUninitialized: false,
+  cookies: {
+    secure: true
+  }
+}))
 
+app.use('/api', apiController)
 app.get('/', (req, res, next) => {
   res.render('home')
 })
-
+app.use('/admin', adminController)
 app.use('*', (req, res, next) => {
   next({
     code: 404,
     msg: 'Page Not Found'
   })
 })
-
 app.use((err, req, res, next) => {
   console.error(err)
   res.locals.err = {
     code: err.code || 500,
     msg: err.msg || 'Internal Server Error'
   }
-  res.render('error')
+  if (req.originalUrl.split('/')[1] === 'api') {
+    res.send(err)
+  } else {
+    res.render('error')
+  }
 })
 
 app.listen(3000, () => {
