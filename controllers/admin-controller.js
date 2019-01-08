e75a9c7818d76fd5b68f78f37851f860fb7ce0f0
sidikriders <sidik@xwork.id>
2019-01-08 18:14:49 +0700

    create dashboard admin page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,16 +1,28 @@
 var router = require('express').Router()
 
 router.get('/login', (req, res, next) => {
-  res.render('admin/login-admin')
+  if (req.session.isLogin) {
+    res.redirect('dashboard')
+  } else {
+    res.render('admin/login-admin')
+  }
 })
 
-// router.get('*', (req, res, next) => {
-//   if (req.session.isLogin) {
+router.get('*', (req, res, next) => {
+  next()
+  // if (req.session.isLogin) {
+  //   next()
+  // } else {
+  //   res.redirect('login')
+  // }
+})
 
-//   }
-// })
-
-router.get('/', (req, res, next) => {
+router.get('/dashboard', (req, res, next) => {
+  res.locals.user = req.session.currUser
+  res.locals.carList = []
+  res.locals.brandList = []
+  res.locals.typeList = []
+  res.locals.transmitionList = []
   res.render('admin/dashboard')
 })
 
